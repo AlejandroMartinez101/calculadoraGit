9e556e626bdef46340706ebcf94ed8bf6a266232
Alejandro <daw112.2023@gmail.com>
2023-01-18 09:47:14 +0100

    Funcion suma

--- src/calculadora.java
+++ src/calculadora.java
@@ -3,7 +3,10 @@ public class calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		calculadora miCalc = new calculadora();
 	}
+	public int sumar(int a, int b) {
+		return a + b;
+		}
 
 }
