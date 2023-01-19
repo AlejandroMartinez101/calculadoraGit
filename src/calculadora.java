764e9ee6fe008d78c5bf7177cdf58102d1282272
Alejandro <daw112.2023@gmail.com>
2023-01-19 09:21:29 +0100

    Probando Clase

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,8 +2,10 @@
 public class calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub+
 		calculadora miCalc = new calculadora();
+		System.out.println(miCalc.sumar(5,3));
+		System.out.println(miCalc.restar(5,3));
 	}
 	public int sumar(int a, int b) {
 		return a + b;
